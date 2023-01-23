d508eef52ad5729f99c10a99fb82aa930632848f
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-01-23 16:19:34 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 /* Перше */
 
-let USER_AGE = Number(prompt('Введіть ваш вік:'));
+/*const USER_AGE = Number(prompt('Введіть ваш вік:'));
 if (USER_AGE > 0 && USER_AGE < 12){
 alert('Ви дитина');
 } else if (USER_AGE >= 12 && USER_AGE < 18){
@@ -11,4 +11,42 @@ alert('Ви дитина');
   alert('Ви пенсіонер');
 } else if (USER_AGE > 120){
   alert('Ви ввели невірний вік');
+}*/
+
+/* Друге */
+
+let USER_NUMBER = Number(prompt('Введіть будь яке число від 0 до 9:'));
+switch (USER_NUMBER) {
+  case 0:
+    alert (')');
+    break;
+    case 1:
+    alert ('!');
+    break;
+    case 2:
+    alert ('@');
+    break;
+    case 3:
+    alert ('#');
+    break;
+    case 4:
+    alert ('$');
+    break;
+    case 5:
+    alert ('%');
+    break;
+    case 6:
+    alert ('^');
+    break;
+    case 7:
+    alert ('&');
+    break;
+    case 8:
+    alert ('*');
+    break;
+    case 9:
+    alert ('(');
+    break;
+    default:
+    alert('Ви ввели невірне число!');
 }
