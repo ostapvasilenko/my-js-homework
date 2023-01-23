e09a666f73042eb77d15745c6f4df910b6abe2b1
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-01-23 15:37:34 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
 /* Перше */
 
 let USER_AGE = Number(prompt('Введіть ваш вік:'));
-if (USER_AGE > 0 && USER_AGE <= 12){
+if (USER_AGE > 0 && USER_AGE < 12){
 alert('Ви дитина');
-} else if (USER_AGE > 12 && USER_AGE <= 18){
+} else if (USER_AGE >= 12 && USER_AGE < 18){
   alert('Ви підліток');
-} else if (USER_AGE > 18 && USER_AGE <= 60){
+} else if (USER_AGE >= 18 && USER_AGE <= 60){
   alert('Ви дорослий');
-} else if (USER_AGE >= 60 && USER_AGE < 120){
+} else if (USER_AGE > 60 && USER_AGE <= 120){
   alert('Ви пенсіонер');
 } else if (USER_AGE > 120){
   alert('Ви ввели невірний вік');
